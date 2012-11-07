610e73065ef6fc050f7e19724aa060f80fca35b7
tamirez3dco <tamir@ez3d.co>
2012-11-07 00:05:23 +0000

    taking empty from file

--- Process_Manager/Manager_Form.cs
+++ Process_Manager/Manager_Form.cs
@@ -58,6 +58,8 @@ namespace Process_Manager
                 {
                     ProcessStartInfo psi = new ProcessStartInfo();
                     psi.FileName = @"C:\Inetpub\ftproot\Rendering_Code\EmptyImagesConstructor\bin\Debug\EmptyImagesConstructor.exe";
+                    String temp = (new JavaScriptSerializer()).Serialize((object[])params_dict["scenes"]);
+                    psi.Arguments = temp.Replace("\"", "\\\"");
                     psi.UseShellExecute = true;
                     Process p = Process.Start(psi);
                     p.WaitForExit();
@@ -142,7 +144,6 @@ namespace Process_Manager
                 }
                 return true;
             }
-
             catch (Exception e)
             {
                 MessageBox.Show(e.Message);
