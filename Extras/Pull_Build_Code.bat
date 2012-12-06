0f4f3536e7d3f012aabe18ae807dadf06a91df44
tamirez3dco <tamir@ez3d.co>
2012-12-06 13:53:11 +0000

    Stupid bug - wrong solution name

--- Extras/Pull_Build_Code.bat
+++ Extras/Pull_Build_Code.bat
@@ -6,9 +6,9 @@ echo PULLING...
 git pull
 echo PULLED
 echo CLEANING...
-"C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe" /clean Debug Solution.sln
+"C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe" /clean Debug EZ3D.sln
 echo CLEANED
 echo BUILDING...
-"C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe" /build Debug Solution.sln
+"C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe" /build Debug EZ3D.sln
 echo BUILT
 pause
