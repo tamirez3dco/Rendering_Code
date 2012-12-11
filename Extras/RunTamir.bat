9bb18effe12905f5bb4fde365caaed6ff3bcb41c
tamirez3dco <tamir@ez3d.co>
2012-12-11 04:17:19 +0000

    stam

--- Extras/RunTamir.bat
+++ Extras/RunTamir.bat
@@ -3,5 +3,5 @@ taskkill /F /IM Runer_Process.exe
 taskkill /F /IM Rhino4.exe
 taskkill /F /IM %name%.exe
 cd ..\%name%\bin\Debug
-%name%.exe "{\"disable_low_priority\":true,\"overide_aws_userdata\":true,\"skip_empty_check\":true,\"stopOnERR\":false,\"refresh_rhino_data\":true,\"rhino_visible\":false,\"mult\":2,\"scenes\":[\"pendants_testing\",\"cases_testing\"],\"name\":\"deploy\",\"timeout\":90}
+%name%.exe "{\"activate_monitor\":true,\"disable_low_priority\":true,\"overide_aws_userdata\":true,\"skip_empty_check\":true,\"stopOnERR\":false,\"refresh_rhino_data\":true,\"rhino_visible\":false,\"mult\":2,\"scenes\":[\"pendants_testing\",\"cases_testing\"],\"name\":\"deploy\",\"timeout\":90}
 pause
