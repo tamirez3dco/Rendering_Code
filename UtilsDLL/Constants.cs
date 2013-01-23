de4f61ce8db1aa3b9ef072eeb8e55fceac3e4e1f
tamirez3dco <tamir@ez3d.co>
2013-01-23 01:51:35 +0000

    Adding Constants

---
+++ UtilsDLL/Constants.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UtilsDLL
+{
+    public class Constants
+    {
+        public const String PARAM_JSON_KEY = "param";
+        public const String OPERATION_JSON_KEY = "operation";
+        public const String GH_FILE_JSON_KEY = "gh_file";
+        public const String STATUS_JSON_KEY = "status";
+        public const String URL_JSON_KEY = "url";
+        public const String REDUCE_MESH_JSON_KEY = "reduce_mesh";
+        public const String EXPORT_FORMAT_JSON_KEY = "export_format";
+        public const String REDUCE_BY_JSON_KEY = "reduce_by";
+        public const String REDUCE_BY_PERCENTAGE = "percentage";
+        public const String REDUCE_BY_FACES = "faces";
+
+        public const String GHX_ADJUSTING_CMD = "adjust_ghx";
+        public const String RENDER_CMD = "render_model";
+    }
+}
